9a6e03e3ebe545d5bbaca76b927376afe0442d0a
jinee <sksowk156@gmail.com>
2023-04-04 23:16:35 +0900

    OPPLA-17 : feature10, questionlist_menu

---
+++ Oppla/app/src/main/java/com/umc/oppla/data/QuestionListDataTemp.kt
@@ -0,0 +1,8 @@
+package com.umc.oppla.data
+
+data class QuestionListDataTemp(
+    val num : Int,
+    var remainTime:Int,
+    val title:String,
+    val address:String
+)
--- Oppla/app/src/main/java/com/umc/oppla/view/main/question/questionlist/QuestionListFragment.kt
+++ Oppla/app/src/main/java/com/umc/oppla/view/main/question/questionlist/QuestionListFragment.kt
@@ -1,18 +1,55 @@
 package com.umc.oppla.view.main.question.questionlist
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.recyclerview.widget.ConcatAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.umc.oppla.R
 import com.umc.oppla.base.BaseFragment
+import com.umc.oppla.data.QuestionListDataTemp
 import com.umc.oppla.databinding.FragmentQuestionListBinding
+import com.umc.oppla.view.main.question.questiondetail.QuestionDetailFragment
 
+class QuestionListFragment :
+    BaseFragment<FragmentQuestionListBinding>(R.layout.fragment_question_list) {
+    private lateinit var questionlistadapter: QuestionListAdapter
+    private lateinit var notificationAdapter: NotificationAdapter
 
-class QuestionListFragment : BaseFragment<FragmentQuestionListBinding>(R.layout.fragment_question_list) {
     override fun init() {
 
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        questionlistadapter =
+            QuestionListAdapter(object : QuestionListAdapter.onItemQuestionDetailClickInterface {
+                override fun onItemQuestionDetailClick(id: Int, position: Int) {
+                    // 초기 화면
+                    parentFragmentManager
+                        .beginTransaction()
+                        .add(R.id.questionblank_layout, QuestionDetailFragment(), "questiondetail")
+                        .addToBackStack("questionlist")
+                        .commitAllowingStateLoss()
+                }
+            })
+
+        notificationAdapter = NotificationAdapter()
+        val concatAdapter = ConcatAdapter(questionlistadapter, notificationAdapter)
+
+        binding.qustionlistRecyclerviewQuestions.apply {
+            layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
+            adapter = concatAdapter
+        }
+
+        val data = mutableListOf<QuestionListDataTemp>()
+        data.add(QuestionListDataTemp(1, 10, "제목1", "주소~~~"))
+        data.add(QuestionListDataTemp(2, 1, "제목2", "주소~~~"))
+        data.add(QuestionListDataTemp(3, 2, "제목3", "주소~~~"))
+        data.add(QuestionListDataTemp(4, 3, "제목4", "주소~~~"))
+        data.add(QuestionListDataTemp(5, 4, "제목5~~~", "주소~~~"))
+        data.add(QuestionListDataTemp(6, 5, "제목6~~~", "주소~~~"))
+        data.add(QuestionListDataTemp(7, 6, "제목7~~~", "주소~~~"))
+
+        questionlistadapter.submitList(data)
+        notificationAdapter.textview[0] = true
     }
 
 }
