65f3fd38f6cd12ab519a3e5e3a65f38c1e18976a
jinee <sksowk156@gmail.com>
2023-04-28 00:50:10 +0900

    map2

--- Oppla/app/src/main/java/com/umc/oppla/view/main/home/map/MapFragment.kt
+++ Oppla/app/src/main/java/com/umc/oppla/view/main/home/map/MapFragment.kt
@@ -1,15 +1,21 @@
 package com.umc.oppla.view.main.home.map
 
+import android.graphics.Color
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.Observer
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.umc.oppla.R
 import com.umc.oppla.base.BaseFragment
 import com.umc.oppla.data.MarkerDataTemp
 import com.umc.oppla.databinding.FragmentMapBinding
 import com.umc.oppla.view.main.MainActivity
 import com.umc.oppla.viewmodel.LocationViewModel
+import net.daum.mf.map.api.MapCircle
 import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapPoint.mapPointWithGeoCoord
@@ -22,12 +28,17 @@ import java.util.concurrent.Executors
 class MapFragment : BaseFragment<FragmentMapBinding>(com.umc.oppla.R.layout.fragment_map),
     MapViewEventListener, POIItemEventListener {
     lateinit var locationViewModel: LocationViewModel
+    // bottomsheet(매장 상세 정보)
+    private lateinit var bottomSheetBehaviorDoanswer: BottomSheetBehavior<LinearLayout>
+    private lateinit var bottomSheetBehaviorMyquestion: BottomSheetBehavior<LinearLayout>
+
     private val NowMarkers = mutableListOf<MapPOIItem>()
 
     override fun init() {
         binding.apply {
             mapMapview.setMapViewEventListener(this@MapFragment)
             mapMapview.setPOIItemEventListener(this@MapFragment)
+            initializePersistentBottomSheet()
 
             locationViewModel = (activity as MainActivity).locationViewModel
             locationViewModel.mylocation.observe(this@MapFragment, Observer {
@@ -39,6 +50,15 @@ class MapFragment : BaseFragment<FragmentMapBinding>(com.umc.oppla.R.layout.frag
                             it.longitude
                         ), false
                     )
+
+                    val circle1 = MapCircle(
+                        mapPointWithGeoCoord(it.latitude, it.longitude),  // center
+                        500,  // radius
+                        Color.argb(128, 0,191,255),  // strokeColor
+                        Color.argb(120, 130,200,230) // fillColor
+                    )
+                    circle1.tag = 1234
+                    binding.mapMapview.addCircle(circle1)
 
                     // 테스트용-----------------------------------------------------------------------
                     val marker = MapPOIItem()
@@ -54,6 +74,10 @@ class MapFragment : BaseFragment<FragmentMapBinding>(com.umc.oppla.R.layout.frag
                     //------------------------------------------------------------------------------
                 }
             })
+
+            binding.mapImageviewCenterpoint.setOnClickListener {
+                bottomSheetBehaviorDoanswer.state =BottomSheetBehavior.STATE_EXPANDED
+            }
         }
     }
 
@@ -127,6 +151,68 @@ class MapFragment : BaseFragment<FragmentMapBinding>(com.umc.oppla.R.layout.frag
     }
 
     override fun onDraggablePOIItemMoved(p0: MapView?, p1: MapPOIItem?, p2: MapPoint?) {
+    }
+
+    // bottomsheet(매장 상세 정보)
+    private fun initializePersistentBottomSheet() {
+        val dm = resources.displayMetrics
+        // BottomSheetBehavior에 layout 설정
+        bottomSheetBehaviorDoanswer = BottomSheetBehavior.from(binding.mapBottomsheetlayoutDoanswer)
+        // 미리보기 높이 = 현재 위치 버튼 크기 + 로고 크기 + 마진 크기
+        bottomSheetBehaviorDoanswer.peekHeight = 0
+
+        bottomSheetBehaviorDoanswer.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                // BottomSheetBehavior state에 따른 이벤트
+                when (newState) {
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                    }
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+                    }
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {
+                    }
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+        })
+
+        // BottomSheetBehavior에 layout 설정
+        bottomSheetBehaviorMyquestion = BottomSheetBehavior.from(binding.mapBottomsheetlayoutMyquestion)
+        // 미리보기 높이 = 현재 위치 버튼 크기 + 로고 크기 + 마진 크기
+        bottomSheetBehaviorMyquestion.peekHeight = 0
+
+        bottomSheetBehaviorMyquestion.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                // BottomSheetBehavior state에 따른 이벤트
+                when (newState) {
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                    }
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+                    }
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {
+                    }
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+        })
     }
 
 }
